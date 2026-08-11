e1d04b19d1b963ea277dd0d41e8567edf4b1c54e
00username00123 <127809848+00username00123@users.noreply.github.com>
2026-08-11 14:06:23 -0500

    Use native unfiltered collections query for All Libraries

--- lib/data/repositories/collection_repository.dart
+++ lib/data/repositories/collection_repository.dart
@@ -1,7 +1,6 @@
 import 'package:klutter/data/dataproviders/client/api_client.dart';
 import 'package:klutter/data/models/librarydto.dart';
 import 'package:klutter/data/models/pagecollectiondto.dart';
-import 'package:klutter/data/repositories/libraries_repository.dart';
 
 class CollectionsRepository {
   Map<String, List<int>> thumbMap = {};
@@ -9,16 +8,8 @@ class CollectionsRepository {
 
   Future<PageCollectionDto> getCollections(
       int page, LibraryDto? library) async {
-    List<String> libraryIds;
-    if (library != null) {
-      libraryIds = <String>[library.id];
-    } else {
-      final libraries = await LibrariesRepository().getAllLibraries();
-      libraryIds = libraries.map((e) => e.id).toList();
-    }
-
     return await _apiClient.collectionController.getCollections(
-        libraryId: libraryIds.isEmpty ? null : libraryIds,
+        libraryId: library == null ? null : <String>[library.id],
         page: page,
         size: 100);
   }
