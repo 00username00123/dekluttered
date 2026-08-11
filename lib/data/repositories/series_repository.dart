d70dfe80658f49ca3b124a22c79fa97e8ea4b08c
00username00123 <127809848+00username00123@users.noreply.github.com>
2026-08-11 13:22:17 -0500

    Make All Libraries series query explicit

--- lib/data/repositories/series_repository.dart
+++ lib/data/repositories/series_repository.dart
@@ -2,16 +2,26 @@ import 'package:klutter/data/dataproviders/client/api_client.dart';
 import 'package:klutter/data/models/collectiondto.dart';
 import 'package:klutter/data/models/librarydto.dart';
 import 'package:klutter/data/models/pageseriesdto.dart';
+import 'package:klutter/data/repositories/libraries_repository.dart';
 
 class SeriesRepository {
   ApiClient _apiClient = ApiClient();
   Map<String, List<int>> thumbMap = {};
+
   Future<PageSeriesDto> getSeries(
       int page, LibraryDto? library, CollectionDto? collection) async {
+    List<String>? libraryIds;
+    if (library != null) {
+      libraryIds = <String>[library.id];
+    } else {
+      final libraries = await LibrariesRepository().getAllLibraries();
+      libraryIds = libraries.map((e) => e.id).toList();
+    }
+
     return await _apiClient.seriesController.getSeries(
         collectionId: collection == null ? null : [collection.id],
         page: page,
-        libraryId: library == null ? null : [library.id],
+        libraryId: libraryIds == null || libraryIds.isEmpty ? null : libraryIds,
         size: 30);
   }
 
