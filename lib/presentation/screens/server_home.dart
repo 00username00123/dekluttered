bf23903fdd297cf869fd57722e0557a821991874
00username00123 <127809848+00username00123@users.noreply.github.com>
2026-08-12 15:18:24 -0500

    Load home feeds once instead of on rebuild

--- lib/presentation/screens/server_home.dart
+++ lib/presentation/screens/server_home.dart
@@ -13,257 +13,225 @@ import 'package:klutter/presentation/widgets/series_card.dart';
 
 class ServerHome extends StatelessWidget {
   static const routeName = '/serverHome';
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: RepositoryProvider(
-        create: (context) => ServerHomeRepository(),
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text("Server Home"),
-              actions: [KlutterSearchButton()],
-            ),
-            drawer: ServerDrawer(),
-            body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ListView(
-                children: [
-                  KeepReading(),
-                  OnDeck(),
-                  RecentlyaddedSeries(),
-                  RecentlyupdatedSeries(),
-                  RecentlyaddedBooks(),
-                ],
+      child: RepositoryProvider<ServerHomeRepository>(
+        create: (_) => ServerHomeRepository(),
+        child: Builder(
+          builder: (context) {
+            final repository = context.read<ServerHomeRepository>();
+            return MultiBlocProvider(
+              providers: [
+                BlocProvider<KeepReadingCubit>(
+                  create: (_) => KeepReadingCubit(repository)..getKeepReading(),
+                ),
+                BlocProvider<OndeckCubit>(
+                  create: (_) => OndeckCubit(repository)..getOndeck(),
+                ),
+                BlocProvider<RecentlyAddedSeriesCubit>(
+                  create: (_) => RecentlyAddedSeriesCubit(repository)
+                    ..getRecentlyAddedSeries(),
+                ),
+                BlocProvider<RecentlyUpdatedSeriesCubit>(
+                  create: (_) => RecentlyUpdatedSeriesCubit(repository)
+                    ..getRecentlyUpdatedSeries(),
+                ),
+                BlocProvider<RecentlyaddedbooksCubit>(
+                  create: (_) => RecentlyaddedbooksCubit(repository)
+                    ..getRecentlyaddedBooks(),
+                ),
+              ],
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text('Server Home'),
+                  actions: [KlutterSearchButton()],
+                ),
+                drawer: ServerDrawer(),
+                body: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListView(
+                    children: [
+                      KeepReading(),
+                      OnDeck(),
+                      RecentlyaddedSeries(),
+                      RecentlyupdatedSeries(),
+                      RecentlyaddedBooks(),
+                    ],
+                  ),
+                ),
               ),
-            )),
+            );
+          },
+        ),
       ),
     );
   }
 }
 
 class RecentlyupdatedSeries extends StatelessWidget {
-  const RecentlyupdatedSeries({
-    Key? key,
-  }) : super(key: key);
+  const RecentlyupdatedSeries({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<RecentlyUpdatedSeriesCubit, RecentlyUpdatedSeriesState>(
-      bloc: RecentlyUpdatedSeriesCubit(context.read<ServerHomeRepository>())
-        ..getRecentlyUpdatedSeries(),
       builder: (context, state) {
         if (state is RecentlyUpdatedSeriesInitial ||
-            state is RecentlyUpdatedSeriesLoading) {
-          return SizedBox.shrink();
-        } else if (state is RecentlyUpdatedSeriesEmpty) {
+            state is RecentlyUpdatedSeriesLoading ||
+            state is RecentlyUpdatedSeriesEmpty) {
           return SizedBox.shrink();
         } else if (state is RecentlyUpdatedSeriesLoaded) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Recently Updated Series",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 190,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: state.series.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return SeriesCard(state.series[index]);
-                  },
-                ),
-              )
-            ],
+          return _SeriesRail(
+            title: 'Recently Updated Series',
+            series: state.series,
           );
-        } else {
-          return ErrorLoading();
         }
+        return ErrorLoading();
       },
     );
   }
 }
 
 class KeepReading extends StatelessWidget {
-  const KeepReading({
-    Key? key,
-  }) : super(key: key);
+  const KeepReading({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<KeepReadingCubit, KeepReadingState>(
       builder: (context, state) {
-        if (state is KeepReadingInitial || state is KeepReadingLoading) {
-          return SizedBox.shrink();
-        } else if (state is KeepReadingEmpty) {
+        if (state is KeepReadingInitial ||
+            state is KeepReadingLoading ||
+            state is KeepReadingEmpty) {
           return SizedBox.shrink();
         } else if (state is KeepReadingLoaded) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Keep Reading",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 190,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: state.books.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return BookCard(state.books[index]);
-                  },
-                ),
-              ),
-            ],
-          );
-        } else {
-          return ErrorLoading();
+          return _BookRail(title: 'Keep Reading', books: state.books);
         }
+        return ErrorLoading();
       },
-      bloc: KeepReadingCubit(context.read<ServerHomeRepository>())
-        ..getKeepReading(),
     );
   }
 }
 
 class RecentlyaddedSeries extends StatelessWidget {
-  const RecentlyaddedSeries({
-    Key? key,
-  }) : super(key: key);
+  const RecentlyaddedSeries({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<RecentlyAddedSeriesCubit, RecentlyAddedSeriesState>(
       builder: (context, state) {
         if (state is RecentlyAddedSeriesInitial ||
-            state is RecentlyAddedSeriesLoading) {
-          return SizedBox.shrink();
-        } else if (state is RecentlyAddedSeriesEmpty) {
+            state is RecentlyAddedSeriesLoading ||
+            state is RecentlyAddedSeriesEmpty) {
           return SizedBox.shrink();
         } else if (state is RecentlyAddedSeriesLoaded) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Recently Added Series",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 190,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: state.series.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return SeriesCard(state.series[index]);
-                  },
-                ),
-              ),
-            ],
+          return _SeriesRail(
+            title: 'Recently Added Series',
+            series: state.series,
           );
-        } else {
-          return ErrorLoading();
         }
+        return ErrorLoading();
       },
-      bloc: RecentlyAddedSeriesCubit(context.read<ServerHomeRepository>())
-        ..getRecentlyAddedSeries(),
     );
   }
 }
 
 class RecentlyaddedBooks extends StatelessWidget {
-  const RecentlyaddedBooks({
-    Key? key,
-  }) : super(key: key);
+  const RecentlyaddedBooks({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<RecentlyaddedbooksCubit, RecentlyaddedbooksState>(
       builder: (context, state) {
         if (state is RecentlyaddedbooksInitial ||
-            state is RecentlyaddedbooksLoading) {
+            state is RecentlyaddedbooksLoading ||
+            state is RecentlyaddedbooksEmpty) {
           return SizedBox.shrink();
         } else if (state is RecentlyaddedbooksLoaded) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Recently Added Books",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 190,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                    return BookCard(state.books[index]);
-                  },
-                  itemCount: state.books.length,
-                ),
-              ),
-            ],
+          return _BookRail(
+            title: 'Recently Added Books',
+            books: state.books,
           );
-        } else if (state is RecentlyaddedbooksEmpty) {
-          return SizedBox.shrink();
-        } else {
-          return ErrorLoading();
         }
+        return ErrorLoading();
       },
-      bloc: RecentlyaddedbooksCubit(context.read<ServerHomeRepository>())
-        ..getRecentlyaddedBooks(),
     );
   }
 }
 
 class OnDeck extends StatelessWidget {
-  const OnDeck({
-    Key? key,
-  }) : super(key: key);
+  const OnDeck({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<OndeckCubit, OndeckState>(
       builder: (context, state) {
-        if (state is OndeckInitial || state is OndeckLoading) {
+        if (state is OndeckInitial ||
+            state is OndeckLoading ||
+            state is OnDeckEmpty) {
           return SizedBox.shrink();
         } else if (state is OndeckLoaded) {
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "On Deck",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Container(
-                height: 190,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: state.books.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return BookCard(state.books[index]);
-                    }),
-              ),
-            ],
-          );
-        } else if (state is OnDeckEmpty) {
-          return SizedBox.shrink();
-        } else {
-          return ErrorLoading();
+          return _BookRail(title: 'On Deck', books: state.books);
         }
+        return ErrorLoading();
       },
-      bloc: OndeckCubit(context.read<ServerHomeRepository>())..getOndeck(),
+    );
+  }
+}
+
+class _BookRail extends StatelessWidget {
+  final String title;
+  final List books;
+
+  const _BookRail({required this.title, required this.books});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(title, style: Theme.of(context).textTheme.headline6),
+        ),
+        SizedBox(
+          height: 190,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: books.length,
+            cacheExtent: 500,
+            itemBuilder: (context, index) => BookCard(books[index]),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SeriesRail extends StatelessWidget {
+  final String title;
+  final List series;
+
+  const _SeriesRail({required this.title, required this.series});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(title, style: Theme.of(context).textTheme.headline6),
+        ),
+        SizedBox(
+          height: 190,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: series.length,
+            cacheExtent: 500,
+            itemBuilder: (context, index) => SeriesCard(series[index]),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -271,16 +239,11 @@ class OnDeck extends StatelessWidget {
 class ErrorLoading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Icon(
-            Icons.error,
-            color: Colors.red,
-          ),
-          Text("Error loading")
-        ],
-      ),
+    return Column(
+      children: [
+        Icon(Icons.error, color: Colors.red),
+        Text('Error loading'),
+      ],
     );
   }
 }
